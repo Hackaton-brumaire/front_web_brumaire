6da56696036ffb76f296a80ed57d3affb7fce1c8
tbriand <briandthibaud99@gmail.com>
2022-03-11 13:37:38 +0100

    add fixture FAQ

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -3,7 +3,19 @@ import FaqAccoordion from "../component/faq/FaqAccoordion"
 import Client from "../component/faq/Client";
 
 const Faq = () => {
-    let items = [{question: "bonjour1", response: "cava1"}, {question: "bonjour2", response: "cava2"}]
+    let items = [{question: "Mon scooter ne démarre plus ?", response: " Branche la batterie"},
+        {question: "J'ai un problème avec la batterie, la batterie ne recharge plus ? ",
+            response: "Pour commerncer verifier que vous avez correctement brancher la batterie, ci cela ne fonctionne pas conctacter le SAV (ou paye)"},
+        {question: "comment recharger mon scooter ?", response:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac odio accumsan, tincidunt ex in, blandit metus. Aenean erat nulla, faucibus in pharetra congue, interdum vel lectus. Praesent lacus nunc, posuere id nisi eget, euismod rutrum odio. Mauris fringilla, mi et tristique mattis, libero mauris laoreet nisl, vitae vulputate eros. "},
+        {question: "comment recharger mon scooter ?", response:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac odio accumsan, tincidunt ex in, blandit metus. Aenean erat nulla, faucibus in pharetra congue, interdum vel lectus. Praesent lacus nunc, posuere id nisi eget, euismod rutrum odio. Mauris fringilla, mi et tristique mattis, libero mauris laoreet nisl, vitae vulputate eros. "},
+        {question: "comment recharger mon scooter ?", response:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac odio accumsan, tincidunt ex in, blandit metus. Aenean erat nulla, faucibus in pharetra congue, interdum vel lectus. Praesent lacus nunc, posuere id nisi eget, euismod rutrum odio. Mauris fringilla, mi et tristique mattis, libero mauris laoreet nisl, vitae vulputate eros. "},
+        {question: "comment recharger mon scooter ?", response:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac odio accumsan, tincidunt ex in, blandit metus. Aenean erat nulla, faucibus in pharetra congue, interdum vel lectus. Praesent lacus nunc, posuere id nisi eget, euismod rutrum odio. Mauris fringilla, mi et tristique mattis, libero mauris laoreet nisl, vitae vulputate eros. "},
+        {question: "comment recharger mon scooter ?", response:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac odio accumsan, tincidunt ex in, blandit metus. Aenean erat nulla, faucibus in pharetra congue, interdum vel lectus. Praesent lacus nunc, posuere id nisi eget, euismod rutrum odio. Mauris fringilla, mi et tristique mattis, libero mauris laoreet nisl, vitae vulputate eros. "}]
     return (
         <div className="faq--view">
             <h3> Frequanetly Asked Question (FAQ) </h3>
@@ -23,4 +35,3 @@ export default Faq
 
 
 
-   
